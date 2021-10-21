783eebbabeeca79b2332dd6a48c9b7420cd1d310
Michał Mąka <maka-michal@wp.pl>
2021-10-21 23:48:00 +0200

    First playing with getting data from server

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 function HomeScreen({ navigation }) {
+  const [data, setData] = useState("unknown");
+  fetch('https://pokeapi.co/api/v2/pokemon/3')
+    .then((response) => response.json())
+    .then((json) => { setData(json.name) })
+    .catch((error) => { console.error(error); });
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Hello {data}!</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
@@ -16,10 +21,16 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
